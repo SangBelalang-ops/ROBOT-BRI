b5e36ac0f279e1eed47b1b899d3145504c73cfd6
SangBelalang-ops <sangbelalangsembah@gmail.com>
2026-06-05 15:42:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 import streamlit as st
 import pandas as pd
+import requests
 
-# Masukkan link Google Sheets Anda di sini (setelah di-Publish to Web sebagai CSV)
-# Caranya: File > Share > Publish to web > Pilih format CSV
-SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTkVpVI1olnVwUmSli1Gm88rmmkWk-z0ZlzcXPALnlSJh4u5cLXHR46p4eqVHX_R4iSSkE9bb4PLn8r/pubhtml?gid=1051785416&single=true"
+# Masukkan URL yang Anda copy dari langkah 2 di sini
+API_URL = "https://script.google.com/macros/s/AKfycbyR4ar-u8TwY1fVThk65yEiJkn4RMm9hbjXmiJA2k6PEG4JuEUxDMEgxs6TE0Z-E-DXlA/exec"
 
 st.title("BRI AGAM BOT")
 
-def load_data():
-    return pd.read_csv(SHEET_URL)
-
 try:
-    df = load_data()
-    st.dataframe(df, use_container_width=True)
-except Exception as e:
-    st.write("Belum ada data atau link salah. Coba cek link CSV Anda.")
+    response = requests.get(API_URL).json()
+    df = pd.DataFrame([response])
+    st.table(df)
+except:
+    st.error("Gagal ambil data. Pastikan URL sudah benar.")
